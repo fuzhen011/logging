3dfaf716d5ca616de9b94320f5b356b74d6888b7
silabs-kevin <Zhen.Fu@silabs.com>
2020-08-10 12:11:06 +0800

    initial commit

---
+++ logging.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: logging.h
+    > Author: Kevin
+    > Created Time: 2020-08-10
+    > Description: 
+ ************************************************************************/
+
+#ifndef LOGGING_H
+#define LOGGING_H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif //LOGGING_H
